f4db12918e0b524cfd5799eaf96ba9a1fb60cac2
Purusharth Seth <purusharthseth2@gmail.com>
2025-05-26 18:57:28 +0530

    Video viewing fixed

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -10,7 +10,7 @@ export async function GET() {
         createdAt: 'desc'
       }
     });
-    return NextResponse.json({ message: "Videos fetched successfully", videos }, { status: 200 });
+    return NextResponse.json(videos, { status: 200 });
   }catch(error){
     console.error(error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
